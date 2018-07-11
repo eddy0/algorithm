b39b7a9828f739ceed9bc3e4c20ef02653cb036a
eddy0 <awindy369@gmail.com>
2018-07-10 21:09:03 -0700

    add array list and link list

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # algorithm
-data structure and algorithm
+a conclusion and handbook on data structure and algorithm
+
+
+# How to Implement by JS?
+
+### (ArrayList)[https://github.com/eddy0/algorithm/ArrayList]
+
+- address each individual spaces in the memory
+- look up easy, by obj[index]
+- delete or insert expensive, need to collapse and re-range. `(所有数据要往前, 生成新的 index)`
+
+#### (LinkList)[https://github.com/eddy0/algorithm/linkList]
+
+- with the help of node (很多节点)
+- each node contains value and next node `(只知道上一个节点和下一个节点)`
+- node start with node, hand by hand, end with tail `从头到尾`
+- lookup by index can only go from head `(查找只能从头循环)`
+- delete and insert is super cheap, once found the element, just cut with next `(找到元素之后删除很方便)`
+- finding is less expensive than shifting
---
+++ arrayList.js
@@ -0,0 +1,39 @@
+class ArrayList() {
+    constructor(){
+        this.data = {}
+        this.length = 0
+    }
+
+    push(value) {
+        this.data[this.length] = value
+        this.length++
+    }
+
+    pop() {
+        const ans = this.data[this.length - 1]
+        delete this.data[this.length - 1]
+        this.length--
+        return ans
+    }
+
+    get(index) {
+        return this.data[index]
+    }
+
+    delete(index) {
+        const ans = this.data[index]
+        this._collapseTo(index)
+        return ans
+    }
+
+    _collapseTo(index) {
+        for (var i = index; i < this.data.length; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        // last one will be undefined,  需要删除
+        // this.data[length] = undefined
+        delete this.data[this.length - 1]
+        this.length--
+    }
+
+}
