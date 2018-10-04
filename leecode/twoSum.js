34f095e9311582432eaec0ab5802e8ff86d1cfff
eddy0 <awindy369@gmail.com>
2018-10-04 00:28:46 -0700

    add sum

--- leecode/twoSum.js
+++ leecode/twoSum.js
@@ -1,20 +1,20 @@
-var twoSum = function(nums, target) {
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    let a = nums[i];
-    for (let j = nums.length - 1; j > i; j--) {
-      let b = nums[j];
-      if (a + b === target) {
-        result.push(i, j);
-      }
+var twoSum = function (nums, target) {
+    let result = []
+    for (let i = 0; i < nums.length; i++) {
+        let a = nums[i]
+        for (let j = nums.length - 1; j > i; j--) {
+            let b = nums[j]
+            if (a + b === target) {
+                result.push(i, j)
+            }
+        }
     }
-  }
-  return result;
-};
+    return result
+}
 
 const testTwoSum = () => {
-  let nums = [2, 7, 11, 15];
-  let target = 9;
-  // [0,1]
-  twoSum(nums, target);
-};
+    let nums = [2, 7, 11, 15]
+    let target = 9
+    // [0,1]
+    twoSum(nums, target)
+}
