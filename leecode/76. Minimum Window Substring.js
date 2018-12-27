e51e6f60e8e10c31341e35dce29c24e6a01188e3
eddy <awindy369@gmail.com>
2018-12-26 19:37:51 -0800

    slide window

--- leecode/76. Minimum Window Substring.js
+++ leecode/76. Minimum Window Substring.js
@@ -23,39 +23,49 @@ const log = console.log.bind(console)
  * @return {string}
  */
 
-// 把 t 的每个字母放到 array 里面计数
 
 var minWindow = function(str, t) {
   let array = Array(128).fill(0)
+  // 把 t 的每个字母放到 array 里面计数
   for (let char of t) {
-    n = char.charCodeAt()
+    n = char.charCodeAt() 
     array[n]++
   }
+  // 需要包含的值的数组++
   let from = 0
   let total = t.length
-  let min = Number.MAX_VALUE
-  let offset = 0
+  let min = Infinity
+  let j = 0
+  // for 循环, 如果值在数组中大于 0, 那么 total -= 1, 然后给每个值--, total 记录包含需要的数的个数
+  // 如果全部都包含一次, total 就为 0 了 
+  // 当 total为 0 的时候 进入判断, 先给 j 的array值 +1, 如果大于0了, 所以是 ABC中的一个,  total++, 移动 j, 
+  // 并且跳出 while 去循环里面找下一个相同的字母
+
   for (let i = 0; i < str.length; i++) {
     let n = str[i]
     let index = n.charCodeAt()
+    // 先判断值是不是大于0, 再给值--
     if (array[index] > 0) {
-      array[index]--
       total--
     }
-    while (total == 0) {
-      if (i - offset + 1 < min) {
-        min = i - offset + 1
-        from = offset
+    array[index]--
+    
+    while (total === 0) {
+      // i - j + 1 表示 包含的字母的右边
+      if (i - j + 1 < min) {
+        min = i - j + 1
+        from = j
       }
-      offset++
-      array[offset]++
-      if (array[offset] > 0) {
+      let char = str[j]
+      let code = char.charCodeAt()
+      array[code]++
+      if (array[code] > 0) {
         total++
       }
+      j++
     }
-    return min == Math.MAX_VALUE ? '' : str.slice(from, from + min)
   }
-
+  return min === Infinity? '' : str.slice(from, from + min)
 }
 
 let S = 'ADOBECODEBANC'
